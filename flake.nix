16b4de916af055bd35487646d5388eacef4b4886
os1 <os1@qed.ai>
2025-08-26 09:27:28 +0200

    remove devshell, it was pointless
    Change-Id: I6a6a696430a35c74b7258a42f11d989fe9f81b1b

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     moxidle.url = "github:mox-desktop/moxidle";
     moxnotify.url = "github:mox-desktop/moxnotify";
     moxctl.url = "github:mox-desktop/moxctl";
@@ -11,33 +10,7 @@
       nixpkgs,
       ...
     }@inputs:
-    let
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
-      forAllSystems =
-        function:
-        nixpkgs.lib.genAttrs systems (
-          system:
-          let
-            pkgs = import nixpkgs { inherit system; };
-          in
-          function pkgs
-        );
-    in
     {
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell {
-          buildInputs = builtins.attrValues {
-            inherit (pkgs)
-              nixd
-              nixfmt-rfc-style
-              ;
-          };
-        };
-      });
-
       homeManagerModules = {
         inherit (inputs.moxnotify.homeManagerModules) moxnotify;
         inherit (inputs.moxpaper.homeManagerModules) moxpaper;
